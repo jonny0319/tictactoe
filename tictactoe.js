b5637d828c7e18c43f3067a64f806dac9f0f39e8
Jonny Chung <jonny0319@hotmail.com>
2023-08-20 16:47:39 +0800

    feat: Add recrodPosition() to record each move

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,7 @@
 const table = document.querySelector("#app table")
 let player = 'circle'
+let circlePosition = []
+let crossPosition = []
 
 function drawCircle(cell) {
   cell.innerHTML = "<div class='circle' />"
@@ -13,13 +15,20 @@ function switchPlayer() {
   player = player === 'circle' ? 'cross' : 'circle'
 }
 
+function recordPosition(position) {
+  console.log('position is ', position)
+  player === 'circle' ? circlePosition.push(position) : crossPosition.push(position)
+}
+
 table.addEventListener("click", function onTableClicked(event) {
   const cell = event.target
+  const cellPosition = event.target.dataset.index
   if (event.target.tagName !== "TD") {
     return; // early return 
   }
 
   player === 'circle' ? drawCircle(cell) : drawCross(cell)
+  recordPosition(cellPosition)
   switchPlayer()
 
 })
