c12a57bd4e0290ab29cc247d1dc4e26f84283475
Jonny Chung <jonny0319@hotmail.com>
2023-08-20 18:35:04 +0800

    feat: Add checkWinner() to determine winner

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,20 @@
 const table = document.querySelector("#app table")
+const MIN = 4
 let player = 'circle'
 let circlePosition = []
 let crossPosition = []
+let round = 1
+
+const victoryLine = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]
 
 function drawCircle(cell) {
   cell.innerHTML = "<div class='circle' />"
@@ -16,8 +29,24 @@ function switchPlayer() {
 }
 
 function recordPosition(position) {
-  console.log('position is ', position)
-  player === 'circle' ? circlePosition.push(position) : crossPosition.push(position)
+  // console.log('position is ', position)
+  player === 'circle' ? circlePosition.push(Number(position)) : crossPosition.push(Number(position))
+}
+
+function checkWinner() {
+  if (player === 'circle') {
+    for (let i = 0; i < victoryLine.length; i++) {
+      if (victoryLine[i].every(element => circlePosition.includes(element))) {
+        console.log(`${player} wins`)
+      }
+    }
+  } else {
+    for (let i = 0; i < victoryLine.length; i++) {
+      if (victoryLine[i].every(element => crossPosition.includes(element))) {
+        console.log(`${player} wins`)
+      }
+    }
+  }
 }
 
 table.addEventListener("click", function onTableClicked(event) {
@@ -29,6 +58,12 @@ table.addEventListener("click", function onTableClicked(event) {
 
   player === 'circle' ? drawCircle(cell) : drawCross(cell)
   recordPosition(cellPosition)
+  if (round > MIN) {
+    checkWinner()
+  }
   switchPlayer()
+  round++
 
 })
+
+
